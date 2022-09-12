dd664a33a43ececf5ced31a503e2273bc6d7e914
Samyc2002 <2000200402@iitdh.ac.in>
2022-09-12 11:39:14 +0530

    Started Lab 5

--- Lab5/Add-8-bit.asm
+++ Lab5/Add-8-bit.asm
@@ -1,22 +1,51 @@
 ORG 0000H
 LJMP MAIN ;Jump to main subroutine
 
-ORG 50H
+ORG 30H
 INIT:
-    ;Write initialization commands that include storing the two numbers in respective
-locations and defining Port 1 as output port.
+    ;Write initialization commands that include storing the two numbers in respective locations and defining Port 1 as output port.
+    MOV R0, #40H                ; Setting source address to 40H
+    MOV R1, #50H                ; Setting destinatin address to 50H
+    CLR C                       ; Clearing carry flag
+    MOV P1, #00H                ; Setting up port
 RET
 
 ADDER_8BIT:
     ; Write the addition subroutine
+    MOV A, @R0                  ; Take value from source to registor A
+    ADD A, @R1                  ; Add the numbers
+    JNC SAVE
+    MOV R2, #42H                ; Move 42H address to R2
+    MOV R4, #44H                ; Move 44H address to R4
+SAVE:
+    MOV @R2, A                  ; Store the sum in R2
+    MOV R4, C                   ; Storing Carry in the memory
 RET
 
 NIBBLE_8BIT:
     ;Write the code to extract upper and lower nibbles and to call LED program
+    MOV A, @R2                  ; Moving R2 to A
+    ANI A, #F0H                 ; Computing Upper nibble
+    MOV R5, A                  ; Storing the upper nibble in R5
+    MOV A, @R2                  ; Moving R2 to A
+    ANI A, #0FH                 ; Computing Lower nibble
+    MOV R6, A                  ; Storing the lower nibble in R5
 RET
 
 LED:
     ;Showing the values on LED
+    MOV P1, R5
+    ACALL DELAY
+    MOV P1, #000H
+    ACALL DELAY
+    SJMP LED
+DELAY:
+    MOV R3, #225
+DELAY1:
+    MOV R7, #255
+HERE:
+    DNJZ R3, here
+    DNJZ R2, DELAY1
 RET
 
 ;------------------------------------ Main subroutine------------------------------------
